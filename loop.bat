b9d4ae4e2e4d001a067157fd8f106ada1bd5d2fd
ptxhosting <110702830+ptxhosting@users.noreply.github.com>
2022-08-06 10:16:04 -0300

    Create loop.bat

---
+++ loop.bat
@@ -0,0 +1,6 @@
+@echo off
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Cannot get an army tunnel, ensure Ngrok_Auth_token is right in settings> Secrets> secret archive. Perhaps your previous VM still runs: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
+:check
+ping 127.0.0.1 > nul
+cls
+goto check
